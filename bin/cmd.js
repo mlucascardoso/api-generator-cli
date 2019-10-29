71514cfb570ed2172c892f3052b805799d8d5284
Lucas M Cardoso <clucasmedeiros@gmail.com>
2019-10-28 18:31:08 -0700

    Building project skeleton

---
+++ bin/cmd.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+'use strict'
+
+const yargs = require('yargs');
+const { join, resolve } = require('path');
+const { homepage, version } = require(join(__dirname, '../package.json'));
+const { commands } = require('../lib/core/yargs');
+
+// Switch CWD if specified from options
+const cwd = resolve(yargs.argv.cwd || process.cwd());
+process.chdir(cwd);
+
+// Init CLI commands and options
+commands().forEach(cmd => yargs.command(cmd.command, cmd.desc, cmd.builder, cmd.handler));
+
+yargs
+  .help()
+  .demand(1)
+  .epilog((homepage ? `| Documentation: ${homepage}\n` : '') + (version ? `| Version: ${version}` : ''))
+  .wrap(yargs.terminalWidth())
+  .strict()
+  .argv;
